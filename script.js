1b3934d8e73b797537ece096b472ed6bad6a1728
rushikesh <rushithorst1234@gmail.com>
2025-10-21 13:08:47 +0530

    made new sectiojn Education

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-const cards = document.querySelectorAll('.product-card');
+function scrollToContact() {
+      const contactSection = document.getElementById('contact');
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
 
-cards.forEach(card => {
-  const slides = card.querySelectorAll('.media');
-  let index = 0;
-
-  const showSlide = i => {
-    slides.forEach(s => s.classList.remove('active'));
-    slides[i].classList.add('active');
-  }
-
-  card.querySelector('.next').addEventListener('click', () => {
-    index = (index + 1) % slides.length;
-    showSlide(index);
-  });
-
-  card.querySelector('.prev').addEventListener('click', () => {
-    index = (index - 1 + slides.length) % slides.length;
-    showSlide(index);
-  });
-
-  showSlide(index);
-});
+function showResume() {
+  document.getElementById("pdf-container").style.display = "block";
+}
